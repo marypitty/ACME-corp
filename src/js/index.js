e393c177db8ca1a8ed5a6b9928ffa3e442144344
Maria <marypitta@hotmail.it>
2022-04-13 21:31:29 +0200

    final commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,8 @@
+import gsap from "gsap";
 import "../scss/main.scss"
-   
-//SWIPER
+
+
+//SWIPER x versione mobile
 
 var swiper = new Swiper(".registration-slider", {
     spaceBetween: 30,
@@ -21,19 +23,112 @@ var swiper = new Swiper(".registration-slider", {
 
 
 
-//GSAP ANIMATIONS
-let bomb = TweenMax.from(".radio-img, .reserv-button", {
-    y: 400,
+//GSAP & SCROLLTRIGGER animazioni
+
+gsap.registerPlugin(ScrollTrigger);
+
+
+//bottone prenota ora
+gsap.from(".btn1", {
+     opacity: 0,
+    duration: 2,
+    
+});
+gsap.from(".btn2", {
+    scrollTrigger: {
+        trigger: ".btn2",
+        toggleActions: "restart none restart none",
+    },
     opacity: 0,
     duration: 2,
-    ease: 'elastic.inOut',
+    
+})
 
+gsap.from(".btn3", {
+    scrollTrigger: {
+        trigger: ".btn3",
+        toggleActions: "restart none restart none",
+    },
+    opacity: 0,
+    duration: 2,
+})
+
+//immagine header
+gsap.from(".radio-img", {
+    x: 400,
+    duration: 2,
+    ease: 'bounce'
+});
+
+//scrollbar
+let scrollbar = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".gray-section",
+        start: "top",
+        toggleActions: "restart none restart none",
+    }
+});
+scrollbar.from(".contenuto", { x: 200, opacity: 0, duration: 1 })
+
+
+//willy
+let willy = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".gray-section",
+        start: "top",
+        toggleActions: "restart none none none",
+
+    }
+});
+willy.from(".wp-block-group__inner-container", { x: -200, opacity: 0, duration: 1 })
+
+
+//immagini servizi
+gsap.from(".servizio", {
+    scrollTrigger: {
+        trigger: ".servizio",
+        toggleActions: "restart none none none"
+    },
+    opacity: 0,
+    duration: 5
+});
+
+//Partners logo
+gsap.from(".partner-logo", {
+    scrollTrigger: {
+        trigger: ".partners",
+        toggleActions: "restart none none none"
+    },
+    opacity: 0,
+    duration: 5,
+    ease: 'pop-up'
+});
+
+
+//Chi siamo
+gsap.from(".size-full", {
+    scrollTrigger: {
+        trigger: ".white-bg",
+        toggleActions: "restart none none none"
+    },
+    opacity: 0,
+    duration: 3,
+    ease: 'pop-up'
 });
 
 
 
 
-/* BURGER BUTTON */
+
+
+
+
+
+
+
+
+
+/* BURGER BUTTON (open/close) */
 const drawerBtnElement = document.getElementById('drawer-btn');
 const mobileDrawerElement = document.getElementById('mobile-drawer');
 
@@ -48,7 +143,7 @@ drawerBtnElement.addEventListener('click', toggleDrawer);
 const expireDate = new Date("Apr 18, 2022 18:00:00").getTime();
 
 // Intervallo per aggiornare countdown ogni secondo
-const x = setInterval(function() {
+const x = setInterval(function () {
 
     // Data e ora odierni
     let now = new Date().getTime();
